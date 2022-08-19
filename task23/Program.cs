cbab76d98619ec99a3467e9244c4f00b680d577c
Alexander <jayero@mail.ru>
2022-08-19 18:37:11 +0300

    resolved03

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Cubing(int n)
+{
+    string result = "";
+    for (int i = 1; i <= n; i++)
+    {
+        result += $"{Math.Pow(i, 3)} ";
+    }
+    return result;
+}
+
+Console.WriteLine(Cubing(n));
